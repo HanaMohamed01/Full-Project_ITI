30327898c3ae2a5a1024e946a388b35fe02617cd
Hana Mohamed <elnegeryh@gmail.com>
2025-08-01 13:22:48 +0300

    updates style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,10 @@ import { ContactComponent } from './Components/contact/contact.component';
 import { HomeComponent } from './Components/home/home.component';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 import { CartComponent } from './Components/cart/cart.component';
-
 export const routes: Routes = [
     {path: '' , component: HomeComponent },
     {path:"about", component:AboutComponent},
     {path:"contact", component:ContactComponent},
     {path:"details/:id",component:ProductDetailsComponent},
     {path:"cart", component:CartComponent },
-
 ];
